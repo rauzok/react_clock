42cf77ec2131c4926d7b5748c677f3380f1d6c17
Taras Kozak <kozaktarasofficial@gmail.com>
2021-05-21 14:48:22 +0300

    Updates

---
+++ precache-manifest.b65736fcd711f84fd9fca00acb43a77c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5039d24a0bd236002d6e344e042bb2a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0e74bda15412ef0fce26",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "9bfabb54970d8a1edebf",
+    "url": "./static/js/2.c17dec48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c17dec48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e74bda15412ef0fce26",
+    "url": "./static/js/main.9a3508ef.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
